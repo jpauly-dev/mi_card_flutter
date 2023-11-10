c545a2424cb1f168961c6a9b7645d29e404c5406
Jonathan Pauly <klang337@gmail.com>
2023-11-10 23:14:55 +0100

    Solution 48 - projet micard

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+class CustomFontFamilies {
+  static const pacifico = "pacifico";
+  static const sourceSans3 = "Source Sans 3";
+}
+
+class Images {
+  static const profilePic = "images/pexels-ellie-burgin-18954332.jpg";
+}
+
 void main() {
   runApp(
     MyApp(),
@@ -14,7 +23,84 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage(Images.profilePic),
+                  ),
+                  Text(
+                    "Raoul Merguez",
+                    style: TextStyle(
+                      fontFamily: CustomFontFamilies.pacifico,
+                      color: Colors.white,
+                      fontSize: 22,
+                    ),
+                  ),
+                  Text(
+                    "FLUTTER DEVELOPER",
+                    style: TextStyle(
+                      fontFamily: CustomFontFamilies.sourceSans3,
+                      color: Colors.teal.shade100,
+                      fontSize: 18,
+                      letterSpacing: 2.5,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                  ContactInformationWidget(
+                    iconData: Icons.phone,
+                    text: "+33 611 22 33 44",
+                  ),
+                  ContactInformationWidget(
+                    iconData: Icons.mail,
+                    text: "raoul.merguez@saucisse.com",
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ContactInformationWidget extends StatelessWidget {
+  final IconData iconData;
+  final String text;
+
+  const ContactInformationWidget({
+    super.key,
+    required this.iconData,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      child: ListTile(
+        visualDensity: VisualDensity(vertical: -4, horizontal: -4),
+        leading: Icon(
+          iconData,
+          color: Colors.teal.shade900,
+          size: 18,
+        ),
+        title: Text(
+          text,
+          style: TextStyle(
+            fontFamily: CustomFontFamilies.sourceSans3,
+            color: Colors.teal.shade900,
+            fontSize: 18,
+          ),
+        ),
       ),
     );
   }
